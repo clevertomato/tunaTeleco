63eb5bd5eda21946fbbb97a9d1d96e70cd170b35
Juan Ignacio <j.ignacio_soler@hotmail.com>
2023-09-25 22:26:03 +0200

    v0.8

--- src/pages/components/contacto.tsx
+++ src/pages/components/contacto.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Image from 'next/image';
-import sheeran from '../../img/sheeran.png'
-import barato from '../../img/barato.png'
-import hodor from '../../img/hodor.png'
+import sheeran from '../../img/sheeran2.png'
+import barato from '../../img/barato2.png'
+import hodor from '../../img/hodor2.png'
 
 function Contacto() {
   return (
