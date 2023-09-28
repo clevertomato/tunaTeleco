e6eedf441bc468a9d4f80f5762dc449cbd0de8e3
Juan Ignacio <j.ignacio_soler@hotmail.com>
2023-09-28 23:21:55 +0200

    v0.8

--- src/pages/components/contacto.tsx
+++ src/pages/components/contacto.tsx
@@ -6,52 +6,53 @@ import hodor from '../../img/hodor2.png'
 
 function Contacto() {
   return (
-  <div className="h-contacto w-auto p-8 contacto-bg flex items-center justify-center">
-    <div className='h-full md:h-auto w-full flex md:flex-row flex-col gap-8 py-4'>
-      <div className='h-full w-full md:w-3/5 flex justify-center align-middle items-end flex-col gap-8'>
-        <div className="text-3xl text-white grid place-content-center md:w-3/5 w-full">
-          <h2 className='text-center py-2 border-y-2'>¡Contacta con nosotros!</h2>
-        </div>
-        <div className="h-contacto-form md:w-3/5 w-full form-bg border-2 border-white shadow-lg p-4 py-8 md:p-4 grid gap-4 text-sm">
-          <div className="w-full h-full">
-            <div className="md:h-full flex flex-col justify-between w-full text-sm ">
-              <div className='w-full md:h-full 2xl:h-90 flex flex-col justify-between'>
-                <div className="w-full">
-                  <label className='text-white text-lg'>Nombre:</label>
-                  <input type="text" name="full_name" id="full_name" className="h-10 border mt-1 mb-1 rounded-sm px-4 w-full bg-gray-50" required />
-                </div>
-                <div className="">
-                  <label className='text-white text-lg'>Correo:</label>
-                  <input type="email" name="email" id="email" className="h-10 border mt-1 mb-1 rounded-sm px-4 w-full bg-gray-50" required />
-                </div>
-                
-                <div className="">
-                  <label className='text-white text-lg'>Teléfono:</label>
-                  <input type="tel" name="tel" id="tel" className="h-10 border mt-1 mb-1 rounded-sm px-4 w-full bg-gray-50" placeholder="*Opcional*" />
-                </div>
-<div className='flex align-middle w-full h-full'>
-                <div className="w-full h-auto">
-                  <label className='text-white text-lg'>Mensaje:</label>
-                  <textarea name="text" rows={7} className="border mt-1 rounded-sm px-4 w-full bg-gray-50 required"/>
-                </div>
+  <div className="h-contacto w-auto contacto-bg flex items-center justify-center">
+    <div className='h-contacto-form md:h-auto w-full flex md:flex-row flex-col gap-4 py-4'>
+      <div className='md:w-4/6 flex flex-col justify-between align-middle items-end px-4 md:pr-0 md:py-8'>
+        <div className='h-full md:w-4/6 3xl:w-3/5 w-full flex flex-col justify-between gap-4'>
+          <div className="text-3xl text-white grid place-content-center w-full mt-4 md:m-0">
+            <h2 className='text-center py-2 border-y-2'>¡Contacta con nosotros!</h2>
+          </div>
+          <div className="h-contacto-form  w-full form-bg border-2 border-white shadow-lg p-4 py-8 md:p-4 grid gap-4 text-sm">
+            <div className="w-full h-full">
+              <div className="md:h-full flex flex-col justify-between w-full text-sm ">
+                <div className='w-full md:h-full gap-2 flex flex-col justify-between'>
+                  <div className="w-full">
+                    <label className='text-white text-lg'>Nombre:</label>
+                    <input type="text" name="full_name" id="full_name" className="h-10 border mt-1 mb-1 rounded-sm px-4 w-full bg-gray-50" required />
+                  </div>
+                  <div className="">
+                    <label className='text-white text-lg'>Correo:</label>
+                    <input type="email" name="email" id="email" className="h-10 border mt-1 mb-1 rounded-sm px-4 w-full bg-gray-50" required />
+                  </div>
+                  <div className="">
+                    <label className='text-white text-lg'>Teléfono:</label>
+                    <input type="tel" name="tel" id="tel" className="h-10 border mt-1 mb-1 rounded-sm px-4 w-full bg-gray-50" placeholder="*Opcional*" />
+                  </div>
+                  <div className='flex align-middle w-full h-full'>
+                    <div className="w-full h-auto">
+                      <label className='text-white text-lg'>Mensaje:</label>
+                      <textarea name="text" rows={7} className="border mt-1 rounded-sm px-4 w-full bg-gray-50 required"/>
+                    </div>
+                  </div>
+                  <div className="text-right grid place-content-end mt-2">
+                    <button className="border-2 border-white text-white font-bold py-2 px-4  rounded-sm text-xl hover:shadow-md hover:shadow-my-orange-700 hover:border-my-orange hover:bg-slate-50/5 transition-all duration-300 hover:cursor-pointer"> Enviar  </button>
+                  </div>
                 </div>
               </div>
-              <div className="text-right grid place-content-end">
-                <div className="inline-flex items-end">
-                  <button className="border-2 border-white text-white font-bold py-2 px-4  rounded-sm text-xl hover:shadow-md hover:shadow-my-orange-700 hover:border-my-orange hover:bg-slate-50/5 transition-all duration-300 hover:cursor-pointer"> Enviar  </button>
-                </div>
             </div>
-            </div>
-          </div>
+        </div>
         </div>
       </div>
-      <div className='h-3/5 md:w-2/5 w-full'>
-        <div className='h-full w-full md:w-2/5 flex align-middle items-end justify-between flex-col gap-8'>
-          <div className="text-3xl text-white grid place-content-center w-full">
-            <h2 className='text-center py-2 border-y-2'>¡Llámanos!</h2>
+      <div className='md:w-1/2 flex flex-col justify-between align-middle items-start py-8 md3:pr-28'>
+        <div className="md2:w-5/6 md3:w-4/6 3xl:w-3/6 w-full flex flex-col items-start h-full gap-4 px-4 ">
+          <div className='w-full '>
+            <div className="text-3xl text-white grid place-content-center w-full">
+              <h2 className='text-center py-2 border-y-2'>¡Llámanos!</h2>
+            </div>
           </div>
-          <div className='w-full flex flex-col justify-between text-white h-60'>
-            <div className='form-bg h-30 w-full border-2 flex flex-row'>
+          <div className='w-full h-full flex flex-col justify-between text-white'>
+          <div className='h-full form-bg  md:h-contacto-foto 2xl:h-contacto-foto2  w-full border-2 flex flex-row mb-4 md:m-0'>
                 <div className='h-full w-1/2 border-r-2'>
                       <Image
                       src={sheeran}
@@ -68,7 +69,7 @@ function Contacto() {
                   </li>
                 </ul>
             </div>
-            <div className='form-bg h-30 w-full border-2 flex flex-row m-0'>
+            <div className=' form-bg h-contacto-foto 2xl:h-contacto-foto2 h-min w-full border-2 flex flex-row  mb-4 md:m-0'>
                 <div className='h-full w-1/2 border-r-2'>
                       <Image
                       src={barato}
@@ -85,7 +86,7 @@ function Contacto() {
                   </li>
                 </ul>
             </div>
-            <div className='form-bg h-30 w-full border-2 flex flex-row'>
+            <div className='form-bg h-contacto-foto 2xl:h-contacto-foto2 w-full border-2 flex flex-row'>
                 <div className='h-full w-1/2 border-r-2'>
                       <Image
                       src={hodor}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Main from './components/main/main';
 import Quesomos from './components/quesomos';
 import Valores from './components/valores';
 import Celebraciones from './components/celebraciones';
-import Contacto2 from './components/contacto copy';
+import Contacto from './components/contacto';
 
 export default function Home() {
   const [show, setShow] = useState(true);
@@ -17,7 +17,7 @@ export default function Home() {
       <Quesomos/>
       <Valores/>
       <Celebraciones/>
-      <Contacto2/>
+      <Contacto/>
     </div>
   )
 }
