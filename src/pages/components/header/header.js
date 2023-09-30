b1d41f02f99d35c169fa705052dfe2e86b357d81
Juan Ignacio <j.ignacio_soler@hotmail.com>
2023-09-30 22:51:19 +0200

    v0.8

--- src/pages/components/header/header.js
+++ src/pages/components/header/header.js
@@ -15,7 +15,7 @@ export default function Header({ showNav }) {
     <div id='navBar' className={`${!showNav ? 'opacity-0' : 'opacity-100 '}  z-20  top-0 right-0 w-full transition-all ease-in-out delay-300 duration-500 absolute `}>
       <div className='hidden md:flex items-center justify-between w-full'>
         <div>
-        <div className='flex flex-row text-md md:text-3xl lg:text-4xl 3xl:text-4xl text-black px-4 py-2 float-right transform hover:cursor-pointer font-normal ease-in-out border-b-3 rounded-sm border-transparent border-white hover:border-my-orange hover:shadow-sm transition-all duration-400 ml-4'>
+        <div className='flex flex-row text-md md:text-3xl lg:text-4xl 3xl:text-4xl text-black px-4 py-2 float-right transform hover:cursor-pointer font-normal ease-in-out border-b-3 border-transparent border-white hover:border-my-orange hover:shadow-sm transition-all duration-400 ml-4'>
           <Image
                 src={instagram}
                 width={35}
@@ -41,7 +41,7 @@ export default function Header({ showNav }) {
       </div>
       <div className='flex items-center justify-between md:hidden w-full pt-4'>
         <div>
-        <div className='flex flex-row text-2xl text-black px-4 pb-2 float-right transform hover:cursor-pointer font-normal ease-in-out border-b-3 rounded-sm border-transparent border-white hover:border-my-orange hover:shadow-sm transition-all duration-400 ml-4'>
+        <div className='flex flex-row text-2xl text-black px-4 pb-2 float-right transform hover:cursor-pointer font-normal ease-in-out border-b-3 border-transparent border-white hover:border-my-orange hover:shadow-sm transition-all duration-400 ml-4'>
           <Image
                 src={instagram}
                 width={26}
@@ -61,16 +61,16 @@ export default function Header({ showNav }) {
         </button>
 
       </div>
-      <div className="md:hidden w-full  flex justify-end mt-2 pr-6">
-        <ul className='w-1/2 h-full text-2xl'>
-          <li className={`${openedMenu ? 'opacity-0' : 'opacity-100'} transition-all duration-200 pl-4 py-2 text-right grid place-content-end`}>
-            <p className='border-b-2 border-white pb-1'>CONTACTO</p>
+      <div className="md:hidden w-full  flex flex-row mt-4 px-2">
+        <ul className='w-full h-full text-lg flex flex-row justify-between'>
+          <li className={`${openedMenu ? 'opacity-0' : 'opacity-100'} transition-all delay-200 duration-200 grid place-content-end`}>
+            <p className='border-b-2 border-white  pb-1 text-center'>CONTACTO</p>
           </li>
-          <li className={`${openedMenu ? 'opacity-0' : 'opacity-100'} transition-all delay-200 duration-200 pl-4 py-2 text-right grid place-content-end`}>
-            <p className='border-b-2 border-white pb-1'>CANCIONERO</p>
+          <li className={`${openedMenu ? 'opacity-0' : 'opacity-100'} transition-all delay-200 duration-200  grid place-content-end`}>
+            <p className='border-b-2 border-white  pb-1 text-center'>CANCIONERO</p>
           </li>
-          <li className={`${openedMenu ? 'opacity-0' : 'opacity-100'} transition-all delay-400 duration-150 pl-4 py-2 text-right grid place-content-end`}>
-            <p className='border-b-2 border-white pb-1'>HISTORIA</p>
+          <li className={`${openedMenu ? 'opacity-0' : 'opacity-100'} transition-all delay-200 duration-200 grid place-content-end`}>
+            <p className='border-b-2 border-white  pb-1 text-center px-auto'>HISTORIA</p>
           </li>
           
         </ul>
