83627fd37c4c2fc6b1d023923d1a3a7ce29f4faa
Juan Ignacio <j.ignacio_soler@hotmail.com>
2023-09-30 19:02:47 +0200

    v0.8

--- src/pages/components/valores.tsx
+++ src/pages/components/valores.tsx
@@ -4,58 +4,63 @@ import logoupv from '../../img/UPV.gif'
 
 export default function Valores() {
   return (
-    <div className='h-valores w-full backgroundfixed flex flex-col overflow-x-hidden border-y-2 border-white justify-between py-4 lg:py-0'>
-        <div className='h-2/5 text-white text-xl md:text-2xl md2:text-3xl lg:text-3xl xl:text-4xl grid place-content-center text-center py-4'><p className='border-y-2 pb-2 border-white valores-text '>Nuestros valores</p></div>
-        <ul className='h-3/5 w-full text-white flex flex-col md:flex-row py-4 text-center px-16 lg:px-32 gap-8'>
-            <li className='w-full md:w-1/2 flex flex-row justify-between gap-8'>
-                <div className='h-full w-1/2 md:auto flex flex-col '>
+    <div className='h-auto w-full backgroundfixed flex flex-col overflow-x-hidden border-y-2 border-white gap-4 py-4 lg:py-4'>
+        <div className=' h-2/5 text-white text-xl md:text-2xl md2:text-3xl lg:text-3xl xl:text-4xl grid place-content-center text-center py-4'>
+            <p className='border-y-2 pb-2 border-white valores-text '>
+                Nuestros valores
+            </p>
+        </div>
+        <div className='flex flex-wrap text-white h-3/5'>
+            <div className='w-1/2 md:w-1/4 h-auto flex flex-col p-8 gap-4'>
                 <Image
                         src={logoupv}
                         alt="logoupv"
-                        style={{ height: '100%', width:'100%', marginLeft: 'auto', marginRight: 'auto',  padding: '0.2rem'}}></Image>
-                    <div className='w-full h-full grid place-content-center'>
-                        <p className='text-center text-xl lg:text-xl 3xl:text-3xl  w-fit'>
-                            Mantener la Tradicións
-                        </p>
-                    </div>
+                        height={120}
+                        style={{ width:'auto', marginLeft: 'auto', marginRight: 'auto',  padding: '0.2rem'}}></Image>
+                <div className='w-full h-full grid place-content-center'>
+                    <p className='text-center text-xl lg:text-xl 3xl:text-3xl  w-fit'>
+                        Mantener la Tradición
+                    </p>
                 </div>
-                <div className='h-full w-1/2 md:auto flex flex-col '>
-                    <Image
+            </div>
+            <div className='w-1/2 md:w-1/4 h-auto flex flex-col p-8 gap-4'>
+                <Image
                         src={logoupv}
                         alt="logoupv"
-                        style={{ height: '100%', width:'100%', marginLeft: 'auto', marginRight: 'auto',  padding: '0.2rem'}}></Image>
-                    <div className='w-full h-full grid place-content-center'>
-                        <p className='text-center text-xl lg:text-xl 3xl:text-3xl  w-fit'>
-                            Fomentar el Folcloree
-                        </p>
-                    </div>
+                        height={120}
+                        style={{ width:'auto', marginLeft: 'auto', marginRight: 'auto',  padding: '0.2rem'}}></Image>
+                <div className='w-full h-full grid place-content-center'>
+                    <p className='text-center text-xl lg:text-xl 3xl:text-3xl  w-fit'>
+                        Fomentar el Folclore
+                    </p>
                 </div>
-            </li>
-            <li className='w-full md:w-1/2 flex flex-row gap-8'>
-            <div className='h-full w-1/2 md:auto flex flex-col '>
-                    <Image
+            </div>
+            <div className='w-1/2 md:w-1/4 h-auto flex flex-col p-8 gap-4'>
+                <Image
                         src={logoupv}
                         alt="logoupv"
-                        style={{ height: '100%', width:'100%', marginLeft: 'auto', marginRight: 'auto',  padding: '0.2rem'}}></Image>
-                    <div className='w-full h-full grid place-content-center'>
-                        <p className='text-center text-xl lg:text-xl 3xl:text-3xl  w-fit'>
-                            Vivir nuevas experiencias
-                        </p>
-                    </div>
+                        height={120}
+                        style={{ width:'auto', marginLeft: 'auto', marginRight: 'auto',  padding: '0.2rem'}}></Image>
+                <div className='w-full h-full grid place-content-center'>
+                    <p className='text-center text-xl lg:text-xl 3xl:text-3xl  w-fit'>
+                    Vivir nuevas experiencias
+                    </p>
                 </div>
-                <div className='h-full w-1/2 md:auto flex flex-col '>
-                    <Image
+            </div>
+            <div className='w-1/2 md:w-1/4 h-auto flex flex-col p-8 gap-4'>
+                <Image
                         src={logoupv}
                         alt="logoupv"
-                        style={{ height: '100%', width:'100%', marginLeft: 'auto', marginRight: 'auto',  padding: '0.2rem'}}></Image>
-                    <div className='w-full h-full grid place-content-center'>
-                        <p className='text-center text-xl lg:text-xl 3xl:text-3xl  w-fit'>
-                            Disfrutar de la Vida
-                        </p>
-                    </div>
+                        height={120}
+                        style={{ width:'auto', marginLeft: 'auto', marginRight: 'auto',  padding: '0.2rem'}}></Image>
+                <div className='w-full h-full grid place-content-center'>
+                    <p className='text-center text-xl lg:text-xl 3xl:text-3xl  w-fit'>
+                    Disfrutar de la Vida
+                    </p>
                 </div>
-            </li>
-        </ul>
+            </div>
+            
+        </div>
     </div>
   )
 }
